07937482a0e5a1b20f208b0a3f092d90e8f97434
HT <t.hoang201@gmail.com>
2020-05-18 18:30:49 +0700

    add sayName method

--- Dog.js
+++ Dog.js
@@ -1,3 +1,11 @@
-function Dog (argument) {
-	
+var chalk = require('chalk');
+
+function Dog (name) {
+	this.name = name;
 }
+
+Dog.prototype.sayName = function () {
+	console.log('My name is ' + chalk.red(this.name)); 
+}
+
+module.exports = Dog;
